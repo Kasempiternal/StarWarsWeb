f398b635d6af071f3782834add2724478c06f978
koldoo02 <koldo.moya@opendeusto.es>
2021-11-10 19:12:11 +0100

    retoques

--- StarWarsApp/models.py
+++ StarWarsApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Personaje(models.Model):
+    nombre = models.CharField(max_length= 50)
+    raza = models.CharField(max_length= 50)
+    altura = models.FloatField()
--- StarWarsApp/views.py
+++ StarWarsApp/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.http import HttpResponse
+
+from StarWarsApp.models import Personaje
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+  # personajes = get_list_or_404(Personaje.objects.order_by('altura'))
+# personajeMain = get_object_or_404(Personaje, personaje_id)
+
+   return render(request,'home.html')
