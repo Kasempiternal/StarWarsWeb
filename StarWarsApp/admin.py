c02282f81360fc76745e724bc6b51c87fa5cb7ac
koldoo02 <koldo.moya@opendeusto.es>
2021-11-10 20:23:09 +0100

    bd bd bd bd

--- StarWarsApp/admin.py
+++ StarWarsApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from StarWarsApp.models import Pelicula, Personaje, Raza
+
 # Register your models here.
+admin.site.register(Pelicula)
+admin.site.register(Raza)
+admin.site.register(Personaje)
--- StarWarsApp/models.py
+++ StarWarsApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Pelicula(models.Model):
+ nombre = models.CharField(max_length=40)
+ año = models.IntegerField()
+   
+class Raza(models.Model):
+ nombre = models.CharField(max_length=40)
+ mundo = models.CharField(max_length=40)
+
+class Personaje(models.Model):
+ nombre = models.CharField(max_length=40)
+ raza = models.ForeignKey(Raza, on_delete=models.CASCADE)
+ altura = models.FloatField()
+ peliculas = models.ManyToManyField(Pelicula)
+
+
+
+
+ 
